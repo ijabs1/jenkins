5c528b7b8aa535ddca49a6c664783fc88286d772
Olalekan Asaolu <asaoluolalekan11@outlook.com>
2024-12-24 12:13:46 +0000

    Updated variable.tf

---
+++ terraform_pipeline/variable.tf
@@ -0,0 +1,23 @@
+variable "resource_group_name" {
+  type        = string
+  description = "Name of the resource group"
+}
+
+variable "location" {
+  type        = string
+  description = "Azure region"
+  default     = "eastus"
+}
+
+variable "storage_account_name" {
+  type        = string
+  description = "Name of the storage account"
+}
+
+variable "tags" {
+  type        = map(string)
+  description = "Tags to apply to all resources"
+  default = {
+    environment = "development"
+  }
+}
