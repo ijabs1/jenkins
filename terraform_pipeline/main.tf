056ca0828c4ed9fd890b1331c02a47cf0483d411
Olalekan Asaolu <asaoluolalekan11@outlook.com>
2024-12-24 12:13:16 +0000

    Updated main.tf

--- terraform_pipeline/main.tf
+++ terraform_pipeline/main.tf
@@ -1,1 +1,33 @@
-f
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+  tags     = var.tags
+}
+
+resource "azurerm_storage_account" "storage" {
+  name                     = var.storage_account_name
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  min_tls_version         = "TLS1_2"
+
+  blob_properties {
+    versioning_enabled = true
+  }
+
+  tags = var.tags
+}
