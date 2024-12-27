5b40311d529970ef42375e9212c3a7473df0624a
Olalekan Asaolu <asaoluolalekan11@outlook.com>
2024-12-27 11:19:22 +0000

    Deleted modules

--- modules/vpn_gateway/output.tf
+++
@@ -1,9 +0,0 @@
-output "gateway_id" {
-  description = "The ID of the VPN Gateway"
-  value       = azurerm_virtual_network_gateway.vpn_gateway.id
-}
-
-output "gateway_public_ip" {
-  description = "The public IP of the VPN Gateway"
-  value       = azurerm_public_ip.gateway_pip.ip_address
-}
